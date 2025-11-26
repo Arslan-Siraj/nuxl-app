0e35b2c4cfeab5454666ce4612edba141f6df472
Arslan <arslan.qau11@gmail.com>
2025-11-26 17:47:42 +0100

    added files from rescore res

--- content/rescoring.py
+++ content/rescoring.py
@@ -14,6 +14,53 @@ if 'controllo' not in st.session_state or params["controllo"] == False:
     # Apply captcha by calling the captcha_control function
     captcha_control()
 
+# download the files first time in
+nuxl_rescore_dir: Path = Path(st.session_state.workspace, "nuxl-rescore-files")
+
+from pathlib import Path
+import requests
+import io
+import zipfile
+
+# GitHub release ZIP URL
+zip_url = "https://github.com/Arslan-Siraj/NuXL_rescore_resources/releases/download/0.0.1/nuxl_rescore_resource.zip"
+
+# Check if folder is empty
+if not any(nuxl_rescore_dir.iterdir()):
+    st.info("Resources missing for rescoring. Downloading NuXL rescore resources ...")
+
+    # Progress bar
+    progress_bar = st.progress(0)
+    status_text = st.empty()
+
+    # Streamed download
+    with requests.get(zip_url, stream=True) as r:
+        r.raise_for_status()
+        total_size = int(r.headers.get("Content-Length", 0))
+        downloaded = 0
+        zip_buffer = io.BytesIO()
+
+        for chunk in r.iter_content(chunk_size=8192):
+            if chunk:
+                zip_buffer.write(chunk)
+                downloaded += len(chunk)
+
+                # Update progress bar
+                if total_size > 0:
+                    percent = int(downloaded * 100 / total_size)
+                    progress_bar.progress(percent)
+                    status_text.text(f"Downloading... {percent}%")
+
+    status_text.text("Extracting files...")
+    zip_buffer.seek(0)
+
+    # Extract ZIP contents
+    with zipfile.ZipFile(zip_buffer) as z:
+        z.extractall(nuxl_rescore_dir)
+
+    progress_bar.progress(100)
+    status_text.text("Done!")
+    st.success("Resources downloaded and extracted successfully.")
 
 ##################################
 # Main Rescoring Page Content
@@ -35,11 +82,8 @@ session_files = [
         )
     ]
 
-id_file = r"/home/arslan/test_rescore/RNA_UV_Ecoli_S100_LB_bRfrac_9.idXML"
-calibration = r"/home/arslan/NuXL_rescore/calibration_data/RNA_All.csv"
-unimod = r"/home/arslan/NuXL_rescore/unimod/unimod_to_formula.csv"
-feat_config = r"/home/arslan/NuXL_rescore/features-config.json"
-model_path = r"/home/arslan/NuXL_rescore/RT_deeplc_model/generic_model/full_hc_Train_RNA_All"
+unimod = str(nuxl_rescore_dir / "nuxl_rescore_resource" / "unimod" / "unimod_to_formula.csv")
+feat_config = str(nuxl_rescore_dir / "nuxl_rescore_resource" / "features-config.json")
 output_path = r"/home/arslan/test_rescore/test_out"
 
 with st.form("Rescoring_form", clear_on_submit=False):
@@ -97,23 +141,23 @@ if submit_button:
         else: 
             if Retention_time_features:
                 if protocol == 'RNA_DEB':
-                    model_path = r"/home/arslan/NuXL_rescore/RT_deeplc_model/specific_model/full_hc_Train_RNA_DEB"
-                    calibration_data = r"/home/arslan/NuXL_rescore/calibration_data/RNA_DEB.csv"
+                    model_path = str(nuxl_rescore_dir / "nuxl_rescore_resource" / "RT_deeplc_model" / "specific_model" / "full_hc_Train_RNA_DEB")
+                    calibration_data = str(nuxl_rescore_dir / "nuxl_rescore_resource" / "calibration_data" / "RNA_DEB.csv")
                     st.write("Using RNA_DEB specific model and calibration data.")
 
                 elif protocol == 'RNA_NM':
-                    model_path = r"/home/arslan/NuXL_rescore/RT_deeplc_model/specific_model/full_hc_Train_RNA_NM"
-                    calibration_data = r"/home/arslan/NuXL_rescore/calibration_data/RNA_NM.csv"
+                    model_path = str(nuxl_rescore_dir / "nuxl_rescore_resource" / "RT_deeplc_model" / "specific_model" / "full_hc_Train_RNA_NM")
+                    calibration_data = str(nuxl_rescore_dir / "nuxl_rescore_resource" / "calibration_data" / "RNA_NM.csv")
                     st.write("Using RNA_NM specific model and calibration data.")
 
                 elif protocol == 'RNA_4SU':
-                    model_path = r"/home/arslan/NuXL_rescore/RT_deeplc_model/specific_model/full_hc_Train_RNA_4SU"
-                    calibration_data = r"/home/arslan/NuXL_rescore/calibration_data/RNA_4SU.csv"
+                    model_path = str(nuxl_rescore_dir / "nuxl_rescore_resource" / "RT_deeplc_model" / "specific_model" / "full_hc_Train_RNA_4SU")
+                    calibration_data = str(nuxl_rescore_dir / "nuxl_rescore_resource" / "calibration_data" / "RNA_4SU.csv")
                     st.write("Using RNA_4SU specific model and calibration data.")
 
                 elif protocol == 'RNA_UV' or protocol == "RNA_Other":
-                    model_path = r"/home/arslan/NuXL_rescore/RT_deeplc_model/generic_model/full_hc_Train_RNA_All"
-                    calibration_data = r"/home/arslan/NuXL_rescore/calibration_data/RNA_All.csv"
+                    model_path = str(nuxl_rescore_dir / "nuxl_rescore_resource" / "RT_deeplc_model" / "generic_model" / "full_hc_Train_RNA_All")
+                    calibration_data = str(nuxl_rescore_dir / "nuxl_rescore_resource" / "calibration_data" / "RNA_All.csv")
                     st.write("Using generic RNA_UV and Other model and calibration data.")
             
             st.write(model_path)
@@ -123,17 +167,17 @@ if submit_button:
             if Retention_time_features and not Max_correlation_features:
                 st.write("Using ONLY retention time features.")
                 # Assume 'posix' for Linux and macOS
-                args =["nuxl_rescore", "-id", idXML_file, "-calibration", calibration,
+                args =["nuxl_rescore", "-id", idXML_file, "-calibration", calibration_data,
                      "-unimod", unimod, "-feat_config", feat_config, "-model_path", model_path, "-out", output_path] 
                 
             elif Retention_time_features and Max_correlation_features:
                 st.write("Using retention time and max correlation features.")
                 # Assume 'posix' for Linux and macOS
-                args =["nuxl_rescore", "-id", selected_id_file, "-calibration", calibration,
+                args =["nuxl_rescore", "-id", selected_id_file, "-calibration", calibration_data,
                      "-unimod", unimod, "-feat_config", feat_config, "-model_path", model_path, "-out", output_path]
             #else:
                 # Assume 'posix' for Linux and macOS
-            #    args =["nuxl_rescore", "-id", selected_id_file, "-calibration", calibration,
+            #    args =["nuxl_rescore", "-id", selected_id_file, "-calibration", calibration_data,
             #            "-unimod", unimod, "-feat_config", feat_config, "-model_path", model_path, "-out", output_path]
 
         # Add any additional variables needed for the subprocess (if any)
