73418a92e9c20fa6b89506902d23a237e80df9fb
Arslan <arslan.qau11@gmail.com>
2026-01-16 19:09:21 +0100

    expander and terminate

--- content/dia_library.py
+++ content/dia_library.py
@@ -6,7 +6,6 @@ import os
 from pathlib import Path
 from src.result_files import *
 from src.run_subprocess import *
-from src.view import plot_FDR_plot
 from datetime import datetime
 
 #from nuxl_rescore import run_pipeline
@@ -65,154 +64,180 @@ with st.form("library_generation_form", clear_on_submit=False):
     )
 
     # Submit button
-    submit_button = st.form_submit_button("Generate Library")
+    submit_button = st.form_submit_button("Generate Library", type="primary")
+
+# Create a flag to terminate the subprocess
+terminate_flag = threading.Event()
+terminate_flag.set()
+
+# Function to terminate the subprocess
+def terminate_subprocess():
+      """Set flag to terminate subprocess."""
+      global terminate_flag
+      terminate_flag.set()
 
 if submit_button:
-    if not selected_mzML_files:
-        st.warning("Please select at least one experiment (mzML/raw file) for generating the library.")
-    else:
-      # Extract base names (without extension) for matching
-      mzml_basenames = {os.path.splitext(os.path.basename(f))[0] for f in selected_mzML_files}
+    
+    if st.button("Terminate/Clear", key="terminate-button", type="secondary"):
+      # terminate subprocess
+      terminate_subprocess()
+      st.warning("Process terminated. The analysis may not be complete.")
 
-      # Define the required NuXL result suffixes per mzML
-      required_suffixes = {
-            "_perc_0.0100_XLs.idXML",
-            "_perc_0.0100_peptides.idXML",
-      }
+      # clear form
+      st.rerun() 
 
-      matched_idxmls = []
-      missing_reports = []
+    with st.status("Running analysis... Please wait until analysis done 😑"):
+      if not selected_mzML_files:
+            st.warning("Please select at least one experiment (mzML/raw file) for generating the library.")
+      else:
+            # Extract base names (without extension) for matching
+            mzml_basenames = {os.path.splitext(os.path.basename(f))[0] for f in selected_mzML_files}
 
-      # Check each selected mzML basename
-      for basename in mzml_basenames:
-            # Find all idXML files corresponding to this mzML
-            found = {
-                  f for f in session_idXML_files
-                  if os.path.basename(f).startswith(basename)
-                  and any(f.endswith(suf) for suf in required_suffixes)
+            # Define the required NuXL result suffixes per mzML
+            required_suffixes = {
+                  "_perc_0.0100_XLs.idXML",
+                  "_perc_0.0100_peptides.idXML",
             }
 
-            # The expected filenames for this mzML
-            expected = {basename + suf for suf in required_suffixes}
+            matched_idxmls = []
+            missing_reports = []
 
-            if found != expected:
-                  # Report missing files for this mzML
-                  missing = expected - found
-                  missing_reports.append({
-                  "mzML": basename,
-                  "missing": ", ".join(sorted(missing)),
-                  })
-            else:
-                  matched_idxmls.extend(found)
+            # Check each selected mzML basename
+            for basename in mzml_basenames:
+                  # Find all idXML files corresponding to this mzML
+                  found = {
+                        f for f in session_idXML_files
+                        if os.path.basename(f).startswith(basename)
+                        and any(f.endswith(suf) for suf in required_suffixes)
+                  }
 
-      # --- Display errors for missing files ---
-      if missing_reports:
-            for report in missing_reports:
-                  st.error(
-                  f"Missing NuXL results for '{report['mzML']}': {report['missing']}. "
-                  "Please run the NuXL search engine first or exclude from selection."
-                  )
-            st.stop()  # Stop the app if required files are missing
+                  # The expected filenames for this mzML
+                  expected = {basename + suf for suf in required_suffixes}
 
-      # --- Display matched idXML files ---
-      if matched_idxmls:
-            with st.expander("Show crrosponding idXML filenames for selected mzML/raw files"):
-                  st.text("\n".join(matched_idxmls))
-            output_folder_library = Path(st.session_state.workspace, "result-files", library_name)
-            output_folder_library.mkdir(parents=True, exist_ok=True)
-      
-      console_output = st.empty()  # placeholder for messages
-      log_text = "Export idXML files to TextExporter format\n"
+                  if found != expected:
+                        # Report missing files for this mzML
+                        missing = expected - found
+                        missing_reports.append({
+                        "mzML": basename,
+                        "missing": ", ".join(sorted(missing)),
+                        })
+                  else:
+                        matched_idxmls.extend(found)
 
-      for idxml_file in matched_idxmls:
-            # Input file: from result-files folder
-            idxml_path_in = Path(st.session_state.workspace, "result-files", idxml_file)
+            # --- Display errors for missing files ---
+            if missing_reports:
+                  for report in missing_reports:
+                        st.error(
+                        f"Missing NuXL results for '{report['mzML']}': {report['missing']}. "
+                        "Please run the NuXL search engine first or exclude from selection."
+                        )
+                  st.stop()  # Stop the app if required files are missing
 
-            # Output file: same folder, stem + .unknown
-            idxml_path_out = output_folder_library / (idxml_path_in.stem + ".unknown")
-            TextExporter_exec = os.path.join(os.getcwd(),'TextExporter')
-            # Append current file being processed
-            log_text += f"Processing idxml_file: {idxml_file}\n"
+            console_output = st.empty()  # placeholder for messages
+            # --- Display matched idXML files ---
+            if matched_idxmls:
+                  log_text = "Corresponding idXML filenames for selected experiment mzML/raw files:\n"
+                  for f in matched_idxmls:
+                        log_text += f"- {f}\n"
+            
+                  # Show filenames
+                  console_output.text(log_text)
+                  output_folder_library = Path(st.session_state.workspace, "result-files", library_name)
+                  output_folder_library.mkdir(parents=True, exist_ok=True)
+
+
+            log_text += "\nExport idXML files to TextExporter format\n"
             console_output.text(log_text)
 
-            if os.name == 'nt':
-                  args_convert = [
-                                    TextExporter_exec,
-                                    "-in", str(idxml_path_in),
-                                    "-out", str(idxml_path_out),
-                                    "-id:peptides_only",
-                                    "-id:add_hit_metavalues","0"
-                              ]
-            else:
-                  args_convert = [
-                                    "TextExporter",
-                                    "-in", str(idxml_path_in),
-                                    "-out", str(idxml_path_out)
-                              ]
+            for idxml_file in matched_idxmls:
+                  # Input file: from result-files folder
+                  idxml_path_in = Path(st.session_state.workspace, "result-files", idxml_file)
 
-            #st.info(f"Running: {' '.join(args_convert)}")
+                  # Output file: same folder, stem + .unknown
+                  idxml_path_out = output_folder_library / (idxml_path_in.stem + ".unknown")
+                  TextExporter_exec = os.path.join(os.getcwd(),'TextExporter')
+                  # Append current file being processed
+                  log_text += f"Processing idxml_file: {idxml_file}\n"
+                  console_output.text(log_text)
+
+                  if os.name == 'nt':
+                        args_convert = [
+                                          TextExporter_exec,
+                                          "-in", str(idxml_path_in),
+                                          "-out", str(idxml_path_out),
+                                          "-id:peptides_only",
+                                          "-id:add_hit_metavalues","0"
+                                    ]
+                  else:
+                        args_convert = [
+                                          "TextExporter",
+                                          "-in", str(idxml_path_in),
+                                          "-out", str(idxml_path_out)
+                                    ]
+
+                  #st.info(f"Running: {' '.join(args_convert)}")
+
+                  result = subprocess.run(
+                        args_convert,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                        text=True
+                  )
+
+                  if result.returncode != 0:
+                        console_output.text("idXML → TextExporter conversion failed")
+                        st.text(result.stderr)
+                        st.stop()
+                  else:
+                        log_text += f"{result.stdout}\n"
+                        console_output.text(log_text)
+
+            log_text += "Generating library\n"
+            console_output.text(log_text)
+
+            # --- Python interpreter (always correct in Streamlit)
+            python_exec = sys.executable
+
+            # --- Script path (src is one level above cwd)
+            BASE_DIR = Path(__file__).resolve().parent.parent
+            Gen_library_cmd = (BASE_DIR / "src" / "nuxl2dia.py").resolve()
+
+            # --- Expand input files explicitly (NO wildcards!)
+            unknown_xls = sorted(output_folder_library.glob("*_XLs.unknown"))
+            unknown_pep = sorted(output_folder_library.glob("*_peptides.unknown"))
+
+            if not unknown_xls or not unknown_pep:
+                  st.error("Required .unknown files not found for library generation.")
+                  st.stop()
+
+            # --- Build arguments
+            args_gen_lib = [
+                  python_exec,
+                  str(Gen_library_cmd),
+                  "-i",
+                  *map(str, unknown_xls),
+                  *map(str, unknown_pep),
+                  "-o",
+                  str(output_folder_library / f"{library_name}.tsv"),
+            ]
+
+            #st.info("Running:\n" + " ".join(args_gen_lib))
 
             result = subprocess.run(
-                  args_convert,
+                  args_gen_lib,
                   stdout=subprocess.PIPE,
                   stderr=subprocess.PIPE,
                   text=True
             )
 
             if result.returncode != 0:
-                  console_output.text("idXML → TextExporter conversion failed")
+                  st.error("Library generation failed")
                   st.text(result.stderr)
                   st.stop()
             else:
-                  log_text += f"{result.stdout}\n"
+                  log_text += result.stdout + "\n"
                   console_output.text(log_text)
-
-      log_text += "Generating library\n"
-      console_output.text(log_text)
-
-      # --- Python interpreter (always correct in Streamlit)
-      python_exec = sys.executable
-
-      # --- Script path (src is one level above cwd)
-      BASE_DIR = Path(__file__).resolve().parent.parent
-      Gen_library_cmd = (BASE_DIR / "src" / "nuxl2dia.py").resolve()
-
-      # --- Expand input files explicitly (NO wildcards!)
-      unknown_xls = sorted(output_folder_library.glob("*_XLs.unknown"))
-      unknown_pep = sorted(output_folder_library.glob("*_peptides.unknown"))
-
-      if not unknown_xls or not unknown_pep:
-            st.error("Required .unknown files not found for library generation.")
-            st.stop()
-
-      # --- Build arguments
-      args_gen_lib = [
-            python_exec,
-            str(Gen_library_cmd),
-            "-i",
-            *map(str, unknown_xls),
-            *map(str, unknown_pep),
-            "-o",
-            str(output_folder_library / f"{library_name}.tsv"),
-      ]
-
-      #st.info("Running:\n" + " ".join(args_gen_lib))
-
-      result = subprocess.run(
-            args_gen_lib,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True
-      )
-
-      if result.returncode != 0:
-            st.error("Library generation failed")
-            st.text(result.stderr)
-            st.stop()
-      else:
-            log_text += result.stdout + "\n"
-            console_output.text(log_text)
-      
+            
       
 
 #include peptides?
